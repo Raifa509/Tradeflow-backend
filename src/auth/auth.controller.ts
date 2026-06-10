053aa646e95fcb4dece1892c0827d2086e331bfc
Raifa509 <fathimathulraifanp@gmail.com>
2026-06-10 23:04:31 +0400

    added auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { LoginDto } from "./dto/login.dto";
+
+@Controller('auth')
+export class AuthController{
+    constructor(
+        private authService:AuthService
+    ){}
+
+    //post /auth/login
+    @Post('login')
+    //Store the request body in a variable called dto, and validate/shape it according to LoginDto
+    async login(@Body() dto:LoginDto){
+        return this.authService.login(dto) //calls the service
+    }
+
+    @Get('me')
+    async getMe(@Req() req:any){
+        return this.authService.getMe(req.user.sub) // gets user id from JWT token
+    }
+}
